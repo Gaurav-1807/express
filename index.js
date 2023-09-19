f2f331a627c3ee82555014b9bb032d40ed22ebd7
gaurav_1807 <gauravambaliya77@gmail.com>
2023-09-19 10:38:30 +0530

    patch delete mathod

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
-const express= require("express");
+const express = require("express");
 const app = express();
 app.use(express.json());
-
-app.get("/",(req, res) => {
-    res.status(200).send("Welcome to Index Page")
-})
-app.get("/signup",(req, res) => {
-    res.status(200).send("Welcome to Signup Page")
-})
-app.get("/login",(req, res) => {
-    res.status(200).send("Welcome to Login Page")
-})
-
-app.post("/signup",(req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body)
-})
-app.post("/login",(req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body)
-})
-app.post("/index",(req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body)
-})
-app.post("/students",(req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body)
-})
+const db = [
+    {
+        name: "test",
+        grid: 1234,
+        course: "frontend",
+    }
+]
 
 
+app.get("/student", (req, res) => {
+    res.status(200).send(db)
+})
+app.post("/student", (req, res) => {
+    let newstudent = {
+        name : req.body.name,
+        grid : req.body.grid,
+        course : req.body.course,
+        id : db.length+1 
+    }
+    db.push(newstudent)
+    res.status(200).send(newstudent)
+})
+app.patch("/student/:id", (req, res) => {
+    res.send("enter student")   
+    let {id} = req.params 
+    
+    let updatedStudent =db.filter(student => student.id == id)
+    updatedStudent[0].name = req.body.name
+    updatedStudent[0].grid = req.body.grid
+    updatedStudent[0].course = req.body.course
 
-app.listen(8080,()=>{
- 
+    res.send(...updatedStudent)
+})
+app.delete("/student/:id", (req, res) => {
+    let {id }= req.params 
+    let deletestudents = db.filter(student => student.id != id)
+    console.log(deletestudents)
+    res.send(...deletestudents)
+})
+
+
+
+app.listen(8080, () => {
+
 })
